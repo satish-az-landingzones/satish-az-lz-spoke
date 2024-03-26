06c5260fd8efa72196fd54f114d7d695b0faddba
satishkarengula <satish.karengula@gmail.com>
2024-03-26 11:06:52 +0800

    create spoke vnet

---
+++ variables.tf
@@ -0,0 +1,16 @@
+# Use variables to customize the deployment
+
+variable "root_id" {
+  type    = string
+  default = "es"
+}
+
+variable "root_name" {
+  type    = string
+  default = "Myle-Solutions"
+}
+
+variable "default_location" {
+  type    = string
+  default = "eastus"
+}
