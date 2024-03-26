e0b8756e0d93cb70ef217a3054f60b1c145ceb10
satishkarengula <satish.karengula@gmail.com>
2024-03-26 23:44:28 +0800

    commented admin_ssh_key

--- spoke.tf
+++ spoke.tf
@@ -61,10 +61,10 @@ resource "azurerm_linux_virtual_machine" "spoke" {
     azurerm_network_interface.spoke.id,
   ]
 
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
+  # admin_ssh_key {
+  #   username   = "adminuser"
+  #   public_key = file("~/.ssh/id_rsa.pub")
+  # }
 
   os_disk {
     caching              = "ReadWrite"
