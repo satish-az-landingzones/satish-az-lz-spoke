8b2234d9bacdae5dc1f4231855ca9e9009c04ff4
satishkarengula <satish.karengula@gmail.com>
2024-04-10 01:27:57 +0800

    destroy spoke

--- key_vault.tf
+++ key_vault.tf
@@ -1,39 +1,39 @@
-data "azurerm_client_config" "current" {
-}
+# data "azurerm_client_config" "current" {
+# }
 
-resource "azurerm_key_vault" "spoke" {
-  name                        = "spoke-key-vault"
-  location                    = azurerm_resource_group.spoke.location
-  resource_group_name         = azurerm_resource_group.spoke.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = true
+# resource "azurerm_key_vault" "spoke" {
+#   name                        = "spoke-key-vault"
+#   location                    = azurerm_resource_group.spoke.location
+#   resource_group_name         = azurerm_resource_group.spoke.name
+#   enabled_for_disk_encryption = true
+#   tenant_id                   = data.azurerm_client_config.current.tenant_id
+#   soft_delete_retention_days  = 7
+#   purge_protection_enabled    = true
 
-  sku_name = "standard"
+#   sku_name = "standard"
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+#   access_policy {
+#     tenant_id = data.azurerm_client_config.current.tenant_id
+#     object_id = data.azurerm_client_config.current.object_id
 
-    key_permissions = [
-      "Get", 
-      "Create",
-      "Update",
-    #   "Delete",
-    ]
+#     key_permissions = [
+#       "Get", 
+#       "Create",
+#       "Update",
+#     #   "Delete",
+#     ]
 
-    secret_permissions = [
-      "Get",
-    #   "Delete",
-      "Set",
-    ]
+#     secret_permissions = [
+#       "Get",
+#     #   "Delete",
+#       "Set",
+#     ]
 
-    storage_permissions = [
-      "Get",
-      "Update",
-    #   "Delete",
-      "Set",
-    ]
-  }
-}
+#     storage_permissions = [
+#       "Get",
+#       "Update",
+#     #   "Delete",
+#       "Set",
+#     ]
+#   }
+# }
--- spoke.tf
+++ spoke.tf
@@ -1,55 +1,55 @@
-resource "azurerm_resource_group" "spoke" {
-  name     = "spoke-resources"
-  location = var.default_location
-}
+# resource "azurerm_resource_group" "spoke" {
+#   name     = "spoke-resources"
+#   location = var.default_location
+# }
 
-resource "azurerm_network_security_group" "spoke" {
-  name                = "spoke-security-group"
-  location            = azurerm_resource_group.spoke.location
-  resource_group_name = azurerm_resource_group.spoke.name
-}
+# resource "azurerm_network_security_group" "spoke" {
+#   name                = "spoke-security-group"
+#   location            = azurerm_resource_group.spoke.location
+#   resource_group_name = azurerm_resource_group.spoke.name
+# }
 
-resource "azurerm_virtual_network" "spoke" {
-  name                = "spoke-network"
-  location            = azurerm_resource_group.spoke.location
-  resource_group_name = azurerm_resource_group.spoke.name
-  address_space       = ["10.1.0.0/16"]
-  dns_servers         = ["10.1.0.4", "10.1.0.5"]
+# resource "azurerm_virtual_network" "spoke" {
+#   name                = "spoke-network"
+#   location            = azurerm_resource_group.spoke.location
+#   resource_group_name = azurerm_resource_group.spoke.name
+#   address_space       = ["10.1.0.0/16"]
+#   dns_servers         = ["10.1.0.4", "10.1.0.5"]
 
-  subnet {
-    name           = "spoke_subnet1"
-    address_prefix = "10.1.1.0/24"
-    security_group = azurerm_network_security_group.spoke.id
-  }
+#   subnet {
+#     name           = "spoke_subnet1"
+#     address_prefix = "10.1.1.0/24"
+#     security_group = azurerm_network_security_group.spoke.id
+#   }
 
-  subnet {
-    name           = "spoke_subnet2"
-    address_prefix = "10.1.2.0/24"
-    security_group = azurerm_network_security_group.spoke.id
-  }
+#   subnet {
+#     name           = "spoke_subnet2"
+#     address_prefix = "10.1.2.0/24"
+#     security_group = azurerm_network_security_group.spoke.id
+#   }
 
-  tags = {
-    environment = "Production"
-  }
-}
+#   tags = {
+#     environment = "Production"
+#   }
+# }
 
-data "azurerm_subnet" "spoke_subnet1" {
-  name                 = "spoke_subnet1"
-  virtual_network_name = azurerm_virtual_network.spoke.name
-  resource_group_name  = azurerm_resource_group.spoke.name
-}
+# data "azurerm_subnet" "spoke_subnet1" {
+#   name                 = "spoke_subnet1"
+#   virtual_network_name = azurerm_virtual_network.spoke.name
+#   resource_group_name  = azurerm_resource_group.spoke.name
+# }
 
-resource "azurerm_network_interface" "spoke" {
-  name                = "spoke-vm-nic"
-  location            = azurerm_resource_group.spoke.location
-  resource_group_name = azurerm_resource_group.spoke.name
+# resource "azurerm_network_interface" "spoke" {
+#   name                = "spoke-vm-nic"
+#   location            = azurerm_resource_group.spoke.location
+#   resource_group_name = azurerm_resource_group.spoke.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.spoke_subnet1.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = data.azurerm_subnet.spoke_subnet1.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
 
 # resource "azurerm_linux_virtual_machine" "spoke" {
 #   depends_on = [ azurerm_key_vault_secret.private_key ]
