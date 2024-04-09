086d21c30a2c4f7e27105ea7b40903c82b8b7b77
satishkarengula <satish.karengula@gmail.com>
2024-04-09 23:57:20 +0800

    provision spoke

--- key_vault.tf
+++ key_vault.tf
@@ -1,35 +1,35 @@
-# data "azurerm_client_config" "current" {
-# }
+data "azurerm_client_config" "current" {
+}
 
-# resource "azurerm_key_vault" "spoke" {
-#   name                        = "spoke-key-vault"
-#   location                    = azurerm_resource_group.spoke.location
-#   resource_group_name         = azurerm_resource_group.spoke.name
-#   enabled_for_disk_encryption = true
-#   tenant_id                   = data.azurerm_client_config.current.tenant_id
-#   soft_delete_retention_days  = 7
-#   purge_protection_enabled    = false
+resource "azurerm_key_vault" "spoke" {
+  name                        = "spoke-key-vault"
+  location                    = azurerm_resource_group.spoke.location
+  resource_group_name         = azurerm_resource_group.spoke.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
 
-#   sku_name = "standard"
+  sku_name = "standard"
 
-#   access_policy {
-#     tenant_id = data.azurerm_client_config.current.tenant_id
-#     object_id = data.azurerm_client_config.current.object_id
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
 
-#     key_permissions = [
-#       "Get", 
-#       "Create",
-#       "Update",
-#     ]
+    key_permissions = [
+      "Get", 
+      "Create",
+      "Update",
+    ]
 
-#     secret_permissions = [
-#       "Get",
-#       "Set",
-#     ]
+    secret_permissions = [
+      "Get",
+      "Set",
+    ]
 
-#     storage_permissions = [
-#       "Get",
-#       "Set",
-#     ]
-#   }
-# }
+    storage_permissions = [
+      "Get",
+      "Set",
+    ]
+  }
+}
--- ssh.tf
+++ ssh.tf
@@ -1,26 +1,26 @@
 
-# resource "azapi_resource" "ssh_public_key" {
-#   type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-#   name      = "sshkey-spoke-dev-eastus-001"
-#   location  = azurerm_resource_group.spoke.location
-#   parent_id = azurerm_resource_group.spoke.id
-# }
+resource "azapi_resource" "ssh_public_key" {
+  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  name      = "sshkey-spoke-dev-eastus-001"
+  location  = azurerm_resource_group.spoke.location
+  parent_id = azurerm_resource_group.spoke.id
+}
 
-# resource "azapi_resource_action" "ssh_public_key_gen" {
-#   type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-#   resource_id = azapi_resource.ssh_public_key.id
-#   action      = "generateKeyPair"
-#   method      = "POST"
+resource "azapi_resource_action" "ssh_public_key_gen" {
+  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  resource_id = azapi_resource.ssh_public_key.id
+  action      = "generateKeyPair"
+  method      = "POST"
 
-#   response_export_values = ["publicKey", "privateKey"]
-# }
+  response_export_values = ["publicKey", "privateKey"]
+}
 
-# resource "azurerm_key_vault_secret" "private_key" {
-#   name         = "private-key"
-#   value        = jsondecode(azapi_resource_action.ssh_public_key_gen.output).privateKey
-#   key_vault_id = azurerm_key_vault.spoke.id
-# }
+resource "azurerm_key_vault_secret" "private_key" {
+  name         = "private-key"
+  value        = jsondecode(azapi_resource_action.ssh_public_key_gen.output).privateKey
+  key_vault_id = azurerm_key_vault.spoke.id
+}
 
-# output "key_data" {
-#   value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
-# }
+output "key_data" {
+  value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+}
